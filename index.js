17f8ee7d7e14f11e3b3761c07c7ab5d225d234e7
amaan1dev <amaan.lko32@gmail.com>
2022-09-12 07:33:05 +0530

    update

--- index.js
+++ index.js
@@ -1,53 +1,43 @@
 const billAmount = document.querySelector("#bill-amount");
-
 const cashGiven = document.querySelector("#cash-given");
-
 const checkButton = document.querySelector("#check-button");
-
 const message = document.querySelector("#error-message");
-
-const availableNotes = [2000, 500, 200, 100, 50, 20, 10, 5, 1];
-
 const noOfNotes = document.querySelectorAll(".no-of-notes")
- 
-checkButton.addEventListener("click", function checkInputValues(){
+
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+checkButton.addEventListener("click", function checkBillAndCashAmount(){
     hideMessage();
-    if (billAmount.value > 0){
+    if(billAmount.value > 0){
         if(cashGiven.value >= billAmount.value){
             const amountToBeReturned = cashGiven.value - billAmount.value;
-            
-        }else{
-            showMessage(
-                "Pay the bill or wash the plates"
-                );
+            calculateChange(amountToBeReturned);
+
+        } else{
+            showMessage("Salman khan wants you to wash plates")
         }
-    } else{
-        showMessage(
-            "Invalid bill amount"
-            );
+
+    }else{
+        showMessage("Invalid bill amount")
     }
 });
 
-function calulateChange(amountToBeReturned){
-    for (let i = 0; i < availableNotes.length; i++){
-        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]
-        );
+function calculateChange(amountToBeReturned){
+    for(let i = 0; i < availableNotes.length; i++){
+        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]); 
         amountToBeReturned %= availableNotes[i];
         noOfNotes[i].innerText = numberOfNotes;
+
     }
 }
 
 function hideMessage(){
     message.style.display = "none";
 }
-
-function showMessage(msg) {
+function showMessage(msg){
     message.style.display = "block";
     message.innerText = msg;
 }
+
+
         
-
-
-
-
-
