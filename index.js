0b0446250a3d9a81698f37ed3067aba984d07373
Amaansyed309 <64450603+Amaansyed309@users.noreply.github.com>
2022-10-03 00:40:01 +0530

    bug fixed

--- index.js
+++ index.js
@@ -1,43 +1,51 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
-const message = document.querySelector("#error-message");
+const errorMessage = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes")
 
-const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+const notes = [2000, 500, 100, 20, 10, 5, 1]
 
-checkButton.addEventListener("click", function checkBillAndCashAmount(){
-    hideMessage();
-    if(billAmount.value > 0){
-        if(cashGiven.value >= billAmount.value){
-            const amountToBeReturned = cashGiven.value - billAmount.value;
-            calculateChange(amountToBeReturned);
+checkButton.addEventListener("click", () => {
+    let bill = Number(billAmount.value)
+    let cash = Number(cashGiven.value)
+    errorMessage.style.display = "none"
+    if (bill > 0) {
 
-        } else{
-            showMessage("Salman khan wants you to wash plates")
+        if (cash > bill) {
+            let amountToBeReturned = cash - bill
+            calculateChange(amountToBeReturned)
+        } else {
+            errorMsg("You need to give more cash. 💵")
         }
 
-    }else{
-        showMessage("Invalid bill amount")
+        if (bill === cash) {
+            errorMsg("Nothing to return!")
+        }
+
+    } else {
+        errorMsg("Enter valid Amount. 🤔")
     }
-});
+
+})
 
 function calculateChange(amountToBeReturned){
-    for(let i = 0; i < availableNotes.length; i++){
-        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]); 
-        amountToBeReturned %= availableNotes[i];
-        noOfNotes[i].innerText = numberOfNotes;
+    for(let i = 0; i < notes.length; i++){
+
+
+        const numberOfNotes = Math.trunc(amountToBeReturned / notes[i])
+        amountToBeReturned = amountToBeReturned % notes[i]
+        noOfNotes[i].innerText = numberOfNotes
 
     }
 }
 
-function hideMessage(){
-    message.style.display = "none";
-}
-function showMessage(msg){
-    message.style.display = "block";
-    message.innerText = msg;
+const errorMsg = message => {
+    errorMessage.style.display = "block"
+    errorMessage.innerText = message
 }
 
 
-        
+
+
+
